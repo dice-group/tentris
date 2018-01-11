0579cc89363ca3c73570c145b9b37f71e31f8308
Alexander Bigerl <alexander@bigerl.eu>
2018-01-12 00:32:37 +0100

    simple hypertrie position calc.

--- PosCalc.hpp
+++ PosCalc.hpp
@@ -15,16 +15,28 @@
 
 class PosCalc {
     uint8_t key_length;
+    uint8_t trie_depth;
     std::vector<bool> previous_pos{key_length};
-    std::vector<uint8_t> pos_diff{key_length};
+    std::vector<uint8_t> key_to_trie{key_length};
+    std::vector<uint8_t> trie_to_key{trie_depth};
 
-    uint8_t key_to_trie_pos(uint8_t key_pos){
-        return key_pos - pos_diff[key_pos];
+public:
+    void used(uint8_t key_pos) {
+        for (uint8_t i = key_pos + uint8_t(1); i < key_length; i++) {
+            key_to_trie[i] -= 1;
+        }
+        trie_depth -= 1;
+        for (uint8_t i = key_pos; i < trie_depth; i++) {
+            key_to_trie[i] += 1;
+        }
     }
 
-    uint8_t trie_to_key_pos(uint8_t trie_pos){
+    uint8_t key_to_trie_pos(uint8_t key_pos) {
+        return key_to_trie[key_pos];
+    }
 
-        return key_pos - pos_diff[key_pos];
+    uint8_t trie_to_key_pos(uint8_t trie_pos) {
+        return trie_to_key[trie_pos];
     }
 
 };
