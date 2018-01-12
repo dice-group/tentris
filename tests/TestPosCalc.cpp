123cec15a7c4bcccf5c4831b1e219412f53e1746
Alexander Bigerl <bigerl@informatik.uni-leipzig.de>
2018-01-12 13:29:38 +0100

    added Boost.test structure

---
+++ tests/TestPosCalc.cpp
@@ -0,0 +1,22 @@
+
+#define BOOST_TEST_MODULE LibSparseTensorTest
+
+//#include <boost/test/unit_test.hpp>
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(TestPosCalc)
+
+
+    BOOST_AUTO_TEST_CASE(test_something) {
+        // seven ways to detect and report the same error:
+        BOOST_CHECK(1);        // #1 continues on error
+
+    }
+
+    BOOST_AUTO_TEST_CASE(test_something_else) {
+        // seven ways to detect and report the same error:
+        BOOST_CHECK(1);        // #1 continues on error
+
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
