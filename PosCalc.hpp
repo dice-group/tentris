60919412fcb943b5e19d609f8cdabfb42155e813
Alexander Bigerl <bigerl@informatik.uni-leipzig.de>
2018-01-12 17:17:40 +0100

    finished implementation of PosCalc

--- PosCalc.hpp
+++ PosCalc.hpp
@@ -13,53 +13,107 @@
 #include <unordered_map>
 #include <stdint-gcc.h>
 #include "boost/variant.hpp"
+#include "HyperTrie.hpp"
 
 using std::vector;
 using std::map;
 
+/**
+ * Provides translation between position in superkeys with subkeys. (1,2,4) for example is a subkey of (1,2,3,4).
+ * And the other way around.
+ */
 class PosCalc {
-    inline static map<vector<bool>, PosCalc> instances{};
+    template<typename T>
+    friend
+    class HyperTrie;
+
+    /**
+     * Holds all instances.
+     */
+    inline static map<vector<bool>, PosCalc *> instances{};
+
+    /**
+     * Private construcor.
+     * @param key_length length of superkeys.
+     */
+    explicit PosCalc(const uint8_t &key_length) : key_length(key_length) {}
+
+    /**
+     * Length of superkeys.
+     */
     uint8_t key_length;
-    std::vector<bool> used_pos{key_length};
-    std::vector<uint8_t> key_to_trie{key_length};
-    std::vector<uint8_t> trie_to_key{key_length};
+    /**
+     * Bitmask for position of the superkeys that are already used and now gone in the subkey.
+     */
+    vector<bool> used_positions = vector<bool>(key_length);
+    /**
+     * Difference between the superkeys's and subkeys's key position.
+     */
+    vector<uint8_t> super_to_sub_diff = vector<uint8_t>(key_length);
+
+    /**
+     * Cache for PosCalcs of subkeys that are 1 shorter.
+     */
+    vector<PosCalc *> next_pos_calcs = vector<PosCalc *>(key_length);
 
 public:
-    PosCalc used(uint8_t key_pos) {
-        auto next_used_pos = used_pos;
-        next_used_pos[key_pos] = 1;
-        return getInstance(used_pos);
+
+    /**
+     * Convert a superkey position to a subkey key position.
+     * @param key_pos superkey position
+     * @return subkey position
+     */
+    inline uint8_t key_to_trie_pos(const uint8_t key_pos) const {
+        return key_pos - super_to_sub_diff[key_pos];
     }
 
-public:
-    PosCalc(const uint8_t &key_length) : key_length(key_length) {}
-
-
-    uint8_t key_to_trie_pos(uint8_t key_pos) {
-        return key_to_trie[key_pos];
+    /**
+     * Convert a subkey position to a superkey position.
+     * @param subkey_pos subkey position
+     * @return superkey position
+     */
+    inline uint8_t trie_to_key_pos(const uint8_t subkey_pos) const {
+        return subkey_pos + super_to_sub_diff[subkey_pos];
     }
 
-    uint8_t trie_to_key_pos(uint8_t trie_pos) {
-        return trie_to_key[trie_pos];
+    /**
+     * PosCalcs of subkeys that has one position less.
+     * @param key_pos position of superkey to be removed.
+     * @return PosCalc like this but without position key_pos.
+     */
+    inline PosCalc *used(const uint8_t key_pos) {
+        PosCalc *child = next_pos_calcs.at(key_pos);
+        if (child == nullptr) {
+            vector<bool> next_used_pos = this->used_positions;
+            next_used_pos[key_pos] = 1;
+            child = getInstance(next_used_pos);
+            next_pos_calcs[key_pos] = child;
+        }
+        return child;
     }
 
-    static PosCalc getInstance(std::vector<bool> &used_pos) {
+    /**
+     * Get an instance for a vector of used positions.
+     * @param used_pos bit vector o used positions.
+     * @return an instance
+     */
+    static PosCalc *getInstance(const vector<bool> &used_pos) {
         auto instance_ = instances.find(used_pos);
         if (instance_ != instances.end()) {
             return instance_->second;
         } else {
-            PosCalc instance{uint8_t(used_pos.size())};
+            PosCalc *instance = new PosCalc(uint8_t(used_pos.size()));
 
             for (uint8_t key_pos = 0; key_pos < used_pos.size(); key_pos++) {
 
                 if (used_pos[key_pos]) {
-                    for (uint8_t i = key_pos; i < instance.key_length; i++) {
-                        instance.key_to_trie[i] -= 1;
-                        instance.key_to_trie[i] += 1;
+                    instance->used_positions[key_pos] = true;
+                    for (uint8_t i = key_pos; i < instance->key_length; i++) {
+                        instance->super_to_sub_diff[i] += 1;
                     }
                 }
             }
-            instances[used_pos] = instance;
+            instances.insert_or_assign(used_pos, instance);
             return instance;
         }
     }
--- tests/TestPosCalc.cpp
+++ tests/TestPosCalc.cpp
@@ -1,22 +1,46 @@
+#include "../PosCalc.hpp"
 
 #define BOOST_TEST_MODULE LibSparseTensorTest
 
-//#include <boost/test/unit_test.hpp>
 #include <boost/test/included/unit_test.hpp>
 
 BOOST_AUTO_TEST_SUITE(TestPosCalc)
 
 
-    BOOST_AUTO_TEST_CASE(test_something) {
-        // seven ways to detect and report the same error:
-        BOOST_CHECK(1);        // #1 continues on error
+    BOOST_AUTO_TEST_CASE(every_different_instance_must_exist_only_once) {
+        vector<bool> vec1 = vector<bool>(3);
 
+        PosCalc *inst000 = PosCalc::getInstance(vec1);
+        PosCalc *inst000x = PosCalc::getInstance(vec1);
+
+        BOOST_CHECK_EQUAL(inst000, inst000x);
+
+        PosCalc *inst010 = inst000x->used(1);
+
+        vec1[1] = true;
+        PosCalc *inst010x = PosCalc::getInstance(vec1);
+
+        BOOST_CHECK_EQUAL(inst010, inst010x);
     }
 
-    BOOST_AUTO_TEST_CASE(test_something_else) {
-        // seven ways to detect and report the same error:
-        BOOST_CHECK(1);        // #1 continues on error
+    BOOST_AUTO_TEST_CASE(check_values) {
+        vector<bool> vec1 = vector<bool>(3);
 
+        PosCalc *inst000 = PosCalc::getInstance(vec1);
+        PosCalc *inst100 = inst000->used(0);
+        BOOST_CHECK_EQUAL(inst100->key_to_trie_pos(1), 0);
+        BOOST_CHECK_EQUAL(inst100->key_to_trie_pos(2), 1);
+
+        BOOST_CHECK_EQUAL(inst100->trie_to_key_pos(0), 1);
+        BOOST_CHECK_EQUAL(inst100->trie_to_key_pos(1), 2);
+
+        PosCalc *inst010 = inst000->used(1);
+        BOOST_CHECK_EQUAL(inst010->key_to_trie_pos(0), 0);
+        BOOST_CHECK_EQUAL(inst010->key_to_trie_pos(2), 1);
+
+        BOOST_CHECK_EQUAL(inst010->trie_to_key_pos(0), 0);
+        BOOST_CHECK_EQUAL(inst010->trie_to_key_pos(1), 2);
     }
+
 
 BOOST_AUTO_TEST_SUITE_END()
