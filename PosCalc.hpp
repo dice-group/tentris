c68dca3f8d8bf3e8409181e62fa28d2e31591f4f
Alexander Bigerl <bigerl@informatik.uni-leipzig.de>
2018-01-12 12:40:12 +0100

    implemented PosCalc

--- PosCalc.hpp
+++ PosCalc.hpp
@@ -11,25 +11,29 @@
 #include <vector>
 #include <variant>
 #include <unordered_map>
+#include <stdint-gcc.h>
 #include "boost/variant.hpp"
 
+using std::vector;
+using std::map;
+
 class PosCalc {
+    inline static map<vector<bool>, PosCalc> instances{};
     uint8_t key_length;
-    uint8_t trie_depth;
-    std::vector<bool> previous_pos{key_length};
+    std::vector<bool> used_pos{key_length};
     std::vector<uint8_t> key_to_trie{key_length};
-    std::vector<uint8_t> trie_to_key{trie_depth};
+    std::vector<uint8_t> trie_to_key{key_length};
 
 public:
-    void used(uint8_t key_pos) {
-        for (uint8_t i = key_pos + uint8_t(1); i < key_length; i++) {
-            key_to_trie[i] -= 1;
-        }
-        trie_depth -= 1;
-        for (uint8_t i = key_pos; i < trie_depth; i++) {
-            key_to_trie[i] += 1;
-        }
+    PosCalc used(uint8_t key_pos) {
+        auto next_used_pos = used_pos;
+        next_used_pos[key_pos] = 1;
+        return getInstance(used_pos);
     }
+
+public:
+    PosCalc(const uint8_t &key_length) : key_length(key_length) {}
+
 
     uint8_t key_to_trie_pos(uint8_t key_pos) {
         return key_to_trie[key_pos];
@@ -37,6 +41,27 @@ public:
 
     uint8_t trie_to_key_pos(uint8_t trie_pos) {
         return trie_to_key[trie_pos];
+    }
+
+    static PosCalc getInstance(std::vector<bool> &used_pos) {
+        auto instance_ = instances.find(used_pos);
+        if (instance_ != instances.end()) {
+            return instance_->second;
+        } else {
+            PosCalc instance{uint8_t(used_pos.size())};
+
+            for (uint8_t key_pos = 0; key_pos < used_pos.size(); key_pos++) {
+
+                if (used_pos[key_pos]) {
+                    for (uint8_t i = key_pos; i < instance.key_length; i++) {
+                        instance.key_to_trie[i] -= 1;
+                        instance.key_to_trie[i] += 1;
+                    }
+                }
+            }
+            instances[used_pos] = instance;
+            return instance;
+        }
     }
 
 };
