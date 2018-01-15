a1994c43867895dc5bfc346d82cc05302e429f84
Alexander Bigerl <bigerl@informatik.uni-leipzig.de>
2018-01-15 16:22:27 +0100

    implemented first HyperTrie test.

---
+++ tests/TestHyperTrie.cpp
@@ -0,0 +1,27 @@
+#include "../HyperTrie.hpp"
+
+#define BOOST_TEST_MODULE LibSparseTensorTest
+
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(TestHyperTrie)
+
+    BOOST_AUTO_TEST_CASE(test_single_write_read) {
+        vector<uint64_t> key{5, 10, 8};
+        short value = 1;
+
+        HyperTrie<short> trie{uint8_t(key.size())};
+
+        trie.set(key, value);
+
+        optional<variant<HyperTrie<short> *, short>> safed_value__ = trie.get(key);
+
+        BOOST_CHECK(safed_value__);
+
+        std::variant<HyperTrie<short> *, short> safed_value_ = *safed_value__;
+
+        short safed_value = std::get<short>(*safed_value__);
+        BOOST_CHECK_EQUAL(safed_value, value);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
