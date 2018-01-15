adaced23b2acb08fc4fc781e964d7549539fbbcb
Alexander Bigerl <bigerl@informatik.uni-leipzig.de>
2018-01-15 16:20:25 +0100

    commented in main again

--- main.cpp
+++ main.cpp
@@ -2,21 +2,21 @@
 #include "HyperTrie.hpp"
 #include "Trie.hpp"
 
-//int main() {
-//    std::cout << "Hello, World!" << std::endl;
-//    Trie<int> trie{3};
-//    std::vector<uint64_t> coords{1, 2, 3};
-//    trie.add(coords, 15);
-//    std::vector<uint64_t> coords2{1, 2, 4};
-//    trie.add(coords2, 16);
-//    std::cout << trie.get(coords) << std::endl;
-//    std::cout << trie.get(coords2) << std::endl;
-//    std::vector<uint64_t> coords3{1, 2, 2};
-//    std::cout << trie.get(coords3) << std::endl;
-//
-//    auto x = new u_int64_t[6];
-//    std::cout << "\n" <<
-//              sizeof(std::vector<double>) << " " <<
-//              sizeof(u_int64_t[3]) << std::endl;
-//    return 0;
-//}
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    Trie<int> trie{3};
+    std::vector<uint64_t> coords{1, 2, 3};
+    trie.add(coords, 15);
+    std::vector<uint64_t> coords2{1, 2, 4};
+    trie.add(coords2, 16);
+    std::cout << trie.get(coords) << std::endl;
+    std::cout << trie.get(coords2) << std::endl;
+    std::vector<uint64_t> coords3{1, 2, 2};
+    std::cout << trie.get(coords3) << std::endl;
+
+    auto x = new u_int64_t[6];
+    std::cout << "\n" <<
+              sizeof(std::vector<double>) << " " <<
+              sizeof(u_int64_t[3]) << std::endl;
+    return 0;
+}
