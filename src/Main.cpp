d30674f18eebf8bcfbb4c5bd2d3615baa048efd3
Afshin Amini <mnafshin@gmail.com>
2018-06-19 18:21:01 +0200

    first Rest service is implemented in healthCheck.hpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,10 @@
 
 #include "einsum/Einsum.hpp"
+#include "web/healthCheck.hpp"
+#include "pistache/endpoint.h"
+#include "pistache/net.h"
+#include "pistache/http.h"
+#include "pistache/router.h"
 
 using sparsetensor::tensor::shape_t;
 using sparsetensor::einsum::raw_subscript;
@@ -9,28 +14,5 @@ using sparsetensor::einsum::einsum;
 using std::vector;
 
 int main() {
-    shape_t shape{2, 2};
-
-    HyperTrieTensor<int> tensor_0{shape};
-    tensor_0.set({0, 0}, 1);
-    tensor_0.set({0, 1}, 2);
-    tensor_0.set({1, 0}, 3);
-    tensor_0.set({1, 1}, 5);
-
-    HyperTrieTensor<int> tensor_1{shape};
-    tensor_1.set({0, 0}, 7);
-    tensor_1.set({0, 1}, 11);
-    tensor_1.set({1, 0}, 13);
-    tensor_1.set({1, 1}, 17);
-
-    vector<HyperTrieTensor<int> *> operands{&tensor_0, &tensor_1};
-
-    vector<raw_subscript> op_sc{{0, 1},
-                                {1, 2}};
-    raw_subscript res_sc{0, 2};
-
-    CrossProductTensor<int> *const result = einsum<int>(operands, op_sc, res_sc);
-    for (const auto &non_zero : *result) {
-        std::cout << non_zero << std::endl;
-    }
+    Pistache::Http::listenAndServe<healthCheck>("*:9999");
 }
