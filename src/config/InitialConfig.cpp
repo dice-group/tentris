d58a84d52311d940c9dacf658ff8faffdfbe7622
Afshin Amini <mnafshin@gmail.com>
2018-07-25 16:18:29 +0200

    Initial Config from command line is handled by get_opt_long

---
+++ src/config/InitialConfig.cpp
@@ -0,0 +1,36 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <getopt.h>
+
+class InitialConfig {
+public:
+    int port = 9080;
+    std::string dataBaseFile;
+
+    InitialConfig(int argc, char *argv[]) {
+        static struct option long_options[] = {
+                {"port", required_argument, nullptr, 'p'},
+                {"file", required_argument, nullptr, 'f'},
+        };
+        int opt = 0;
+        int long_index = 0;
+        while ((opt = getopt_long(argc, argv, "p:f:",
+                                  long_options, &long_index)) != -1) {
+            switch (opt) {
+                case 'p':
+                    port = atoi(optarg);
+                    break;
+                case 'f':
+                    dataBaseFile = optarg;
+                    break;
+                default:
+                    printf("Wrong option is set for running the program");
+                    exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    InitialConfig() = default;
+};
