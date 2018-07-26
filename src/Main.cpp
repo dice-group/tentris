dd8578a281cda976c8b07d904aba8f8d6aa1dffc
Alexander Bigerl <alexander.bigerl@upb.de>
2018-07-26 15:52:36 +0200

    refactored merge of input_options

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,12 +2,16 @@
 #include "tnt/store/TripleStore.hpp"
 #include "tnt/http/SPARQLEndpoint.hpp"
 #include "config/InitialConfig.cpp"
+#include <experimental/filesystem>
 
 
 int main(int argc, char *argv[]) {
     using namespace std;
     using namespace Pistache;
     using namespace tnt::http;
+    using namespace tnt::config;
+    namespace fs = std::experimental::filesystem;
+
 
     InitialConfig initialConfig{argc, argv};
 
@@ -20,8 +24,18 @@ int main(int argc, char *argv[]) {
     cout << "Using " << thr << " threads to handle Requests." << endl;
     _store = new tnt::store::TripleStore{};
     std::unique_ptr<tnt::store::TripleStore> _store_ptr(_store);
-    _store->loadRDF(path_to_nt_file);
-    cout << "nt-file: " << path_to_nt_file << endl;
+    if(not initialConfig.dataBaseFile.empty()){
+        if(fs::is_regular_file(path_to_nt_file)){
+        cout << "nt-file: " << path_to_nt_file << endl;
+        _store->loadRDF(path_to_nt_file);
+        } else{
+            cout << "nt-file: " << path_to_nt_file << " not found." << endl;
+            return EXIT_FAILURE;
+        }
+
+    } else{
+        cout << "No file loaded. Use '-f myntfile.nt' if you want to bulkload a file." << endl;
+    }
     cout << "URI: " << "http://127.0.0.1:" << addr.port() << "/sparql?query" << endl;
 
     auto server = std::make_shared<Http::Endpoint>(addr);
@@ -35,5 +49,5 @@ int main(int argc, char *argv[]) {
 
     std::cout << "Shutdowning server" << std::endl;
     server->shutdown();
-
+    return EXIT_SUCCESS;
 }
--- src/config/InitialConfig.cpp
+++ src/config/InitialConfig.cpp
@@ -4,23 +4,33 @@
 #include <string>
 #include <getopt.h>
 
-class InitialConfig {
-public:
-    uint16_t port = 9080;
-    std::string dataBaseFile;
-
-    InitialConfig(int argc, char *argv[]) {
-        static struct option long_options[] = {
+namespace tnt::config {
+    namespace {
+        struct option long_options[] = {
                 {"port", required_argument, nullptr, 'p'},
                 {"file", required_argument, nullptr, 'f'},
         };
-        int opt = 0;
-        int long_index = 0;
-        while ((opt = getopt_long(argc, argv, "p:f:",
-                                  long_options, &long_index)) != -1) {
+    };
+
+    class InitialConfig {
+
+    public:
+        mutable uint16_t port = 9080;
+        mutable std::string dataBaseFile{};
+
+        InitialConfig(int argc, char *argv[]) {
+            int opt = 0;
+            int long_index = 0;
+            while ((opt = getopt_long(argc, argv, "p:f:", long_options, &long_index)) != -1) {}
             switch (opt) {
-                case 'p':
-                    port = atoi(optarg);
+                case 'p': {
+                    const int raw_port_number = atoi(optarg);
+                    if (0 > raw_port_number or raw_port_number > 65535) {
+                        printf("Port must be in range [0,65535].");
+                        exit(EXIT_FAILURE);
+                    }
+                    port = uint16_t(raw_port_number);
+                }
                     break;
                 case 'f':
                     dataBaseFile = optarg;
@@ -31,6 +41,5 @@ public:
             }
         }
     }
-
-    InitialConfig() = default;
+};
 };
