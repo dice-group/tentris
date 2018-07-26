26288ead9f86bc6923cb46f0d739b9634e46f539
Alexander Bigerl <alexander.bigerl@upb.de>
2018-07-26 15:57:52 +0200

    typo in InitialConfig

--- src/config/InitialConfig.cpp
+++ src/config/InitialConfig.cpp
@@ -21,25 +21,25 @@ namespace tnt::config {
         InitialConfig(int argc, char *argv[]) {
             int opt = 0;
             int long_index = 0;
-            while ((opt = getopt_long(argc, argv, "p:f:", long_options, &long_index)) != -1) {}
-            switch (opt) {
-                case 'p': {
-                    const int raw_port_number = atoi(optarg);
-                    if (0 > raw_port_number or raw_port_number > 65535) {
-                        printf("Port must be in range [0,65535].");
-                        exit(EXIT_FAILURE);
+            while ((opt = getopt_long(argc, argv, "p:f:", long_options, &long_index)) != -1) {
+                switch (opt) {
+                    case 'p': {
+                        const int raw_port_number = atoi(optarg);
+                        if (0 > raw_port_number or raw_port_number > 65535) {
+                            printf("Port must be in range [0,65535].");
+                            exit(EXIT_FAILURE);
+                        }
+                        port = uint16_t(raw_port_number);
                     }
-                    port = uint16_t(raw_port_number);
+                        break;
+                    case 'f':
+                        dataBaseFile = optarg;
+                        break;
+                    default:
+                        printf("Wrong option is set for running the program");
+                        exit(EXIT_FAILURE);
                 }
-                    break;
-                case 'f':
-                    dataBaseFile = optarg;
-                    break;
-                default:
-                    printf("Wrong option is set for running the program");
-                    exit(EXIT_FAILURE);
             }
         }
-    }
-};
+    };
 };
