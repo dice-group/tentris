d95b72a322d88713c8f0dffcdb5b39bbeb7af676
Alexander Bigerl <alexander.bigerl@upb.de>
2018-10-25 15:00:59 +0200

    SPARQLparser: Wrote test that checks if correct queries parse without errors.

--- tests/TestSPARQLParser.cpp
+++ tests/TestSPARQLParser.cpp
@@ -1,18 +1,54 @@
 #include <gtest/gtest.h>
 
+#include <fstream>
+#include <regex>
+#include <experimental/filesystem>
+
 #include "tnt/store/SPARQL/ParsedSPARQL.hpp"
 
+std::vector<std::string> load_queries(std::string query_file_path) {
+    std::vector<std::string> queries{};
+    std::ifstream query_file(query_file_path);
+    std::string query;
+    while (std::getline(query_file, query)) {
+        static std::regex white_spaces{"\\s+"};
+        if (not std::regex_match(query, white_spaces))
+            queries.push_back(query);
+    }
+    return queries;
+}
 
-TEST(TestSPARQLParser, da) {
+bool parse_queries(const std::string &file) {
+    std::vector<std::string> queries = load_queries(file);
+
     using namespace tnt::store::sparql;
-    ParsedSPARQL{"PREFIX  dc:  <http://purl.org/dc/elements/1.1/>"
-                 "PREFIX  ns:  <http://example.org/ns#>"
-                 "SELECT  ?title ?price "
-                 "WHERE   { ?x ns:price true ."
-                 "?x a 1.123 . "
-                 "?v ?p \"cat\".  "
-                 "?v ?price \"cat\"@en . "
-                 "?title ?p \"abc\"^^<http://example.org/datatype#specialDatatype> . }"};
+    bool no_errors = true;
+    std::cout << " ### " << file << std::endl;
+    for (const auto&[line_number, query] : enumerate(queries, 1)) {
+        std::cout << " # " << std::setw(3) << line_number << " " << query << std::endl;
+        try {
+            ParsedSPARQL{query};
+            std::cout << " #     " << "OK" << std::endl;
+        } catch (std::exception ex) {
+            no_errors = false;
+            std::cout << " #     " << "Failed" << std::endl;
+            std::cout << ex.what() << std::endl;
+        }
+    }
+    return no_errors;
+}
+
+TEST(TestSPARQLParser, ParseBenchmarkQueries) {
+
+    const std::string sp2b_file = "queries/sp2b.txt";
+    const std::string dbpedia_file = "queries/DBpedia.txt";
+    ASSERT_TRUE(std::experimental::filesystem::is_regular_file(sp2b_file));
+    ASSERT_TRUE(std::experimental::filesystem::is_regular_file(dbpedia_file));
+
+    bool no_errors = true;
+    no_errors &= parse_queries(sp2b_file);
+    no_errors &= parse_queries(dbpedia_file);
+    ASSERT_TRUE(no_errors);
 }
 
 
