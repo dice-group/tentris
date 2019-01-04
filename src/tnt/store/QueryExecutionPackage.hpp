f70add22515c4cebd6d61ccaa605e25793fc0307
Alexander Bigerl <alexander[at]bigerl.eu>
2019-01-04 16:30:13 +0100

    fixed missing bracket

--- src/tnt/store/QueryExecutionPackage.hpp
+++ src/tnt/store/QueryExecutionPackage.hpp
@@ -82,141 +82,142 @@ namespace tnt::store::cache {
 		}
 
 		void done() {
-			processing.unlock(); // TODO: implement }
+			processing.unlock();
+		}// TODO: implement }
 
-			void canceled() { // TODO: implement
+		void canceled() { // TODO: implement
+		}
+
+	private:
+		/**
+		 * Builds the operator tree for this query.
+		 * @tparam RESULT_TYPE the type returned by the operand tree
+		 * @param slice_keys slice keys to extract the operands from the hypertries. slice_keys and hypertries must be
+		 * of equal length.
+		 * @param subscript the subscript that spans the operator tree.
+		 * @param hypertries a list of hypertries. typically this is a list containing the data base hypertrie multiple
+		 * times.
+		 * @return
+		 */
+		template<typename RESULT_TYPE, typename = typename std::enable_if<is_binding<RESULT_TYPE>::value>::type>
+		std::unique_ptr<OperatorNode<RESULT_TYPE>> buildOperatorTree(const std::vector<SliceKey_t> &slice_keys,
+		                                                             const std::shared_ptr<const Subscript> subscript,
+		                                                             const std::vector<BoolHyperTrie *> &hypertries) {
+			if (auto subsubscripts = subscript->getSubSubscripts(); not subsubscripts.empty()) {
+				return std::unique_ptr<OperatorNode<RESULT_TYPE>>{
+						new CrossProduct<RESULT_TYPE>{subscript, slice_keys, hypertries}};
+			} else {
+				return std::unique_ptr<OperatorNode<RESULT_TYPE>>{
+						new Einsum<RESULT_TYPE>{subscript, slice_keys, hypertries}};
 			}
+		}
 
-			private:
-			/**
-			 * Builds the operator tree for this query.
-			 * @tparam RESULT_TYPE the type returned by the operand tree
-			 * @param slice_keys slice keys to extract the operands from the hypertries. slice_keys and hypertries must be
-			 * of equal length.
-			 * @param subscript the subscript that spans the operator tree.
-			 * @param hypertries a list of hypertries. typically this is a list containing the data base hypertrie multiple
-			 * times.
-			 * @return
-			 */
-			template<typename RESULT_TYPE, typename = typename std::enable_if<is_binding<RESULT_TYPE>::value>::type>
-			std::unique_ptr<OperatorNode<RESULT_TYPE>> buildOperatorTree(const std::vector<SliceKey_t> &slice_keys,
-			                                                             const std::shared_ptr<const Subscript> subscript,
-			                                                             const std::vector<BoolHyperTrie *> &hypertries) {
-				if (auto subsubscripts = subscript->getSubSubscripts(); not subsubscripts.empty()) {
-					return std::unique_ptr<OperatorNode<RESULT_TYPE>>{
-							new CrossProduct<RESULT_TYPE>{subscript, slice_keys, hypertries}};
-				} else {
-					return std::unique_ptr<OperatorNode<RESULT_TYPE>>{
-							new Einsum<RESULT_TYPE>{subscript, slice_keys, hypertries}};
-				}
-			}
+	public:
+		const ParsedSPARQL &getParsedSPARQL() const { return parsedSPARQL; }
 
-			public:
-			const ParsedSPARQL &getParsedSPARQL() const { return parsedSPARQL; }
+		const std::string &getSparqlStr() const { return parsedSPARQL.getSparqlStr(); }
 
-			const std::string &getSparqlStr() const { return parsedSPARQL.getSparqlStr(); }
+		const OperatorNode<distinct_binding> &getDistinctOpTree() const {
+			if (is_distinct)
+				return *distinct_operator_tree;
+			else
+				throw std::domain_error("This Packackage holds a non-distinct Operator tree.");
+		}
 
-			const OperatorNode<distinct_binding> &getDistinctOpTree() const {
-				if (is_distinct)
-					return *distinct_operator_tree;
+		const OperatorNode<counted_binding> &getRegularOpTree() const {
+			if (not is_distinct)
+				return *regular_operator_tree;
+			else
+				throw std::domain_error("This Packackage holds a distinct Operator tree.");
+		}
+
+		/**
+		 * Returns an generator for the result if this->is_distinct
+		 * @return result generator
+		 */
+		yield_pull<distinct_binding> getDistinctGenerator() const {
+			if (is_distinct) {
+				if (not is_trivial_emtpy)
+					return distinct_operator_tree->get();
 				else
-					throw std::domain_error("This Packackage holds a non-distinct Operator tree.");
-			}
+					return yield_pull<distinct_binding>(
+							[&]([[maybe_unused]] yield_push<distinct_binding> &yield) { return; });
+			} else
+				throw std::domain_error("This Packackage holds a non-distinct Operator tree.");
+		}
 
-			const OperatorNode<counted_binding> &getRegularOpTree() const {
-				if (not is_distinct)
-					return *regular_operator_tree;
+		/**
+		 * Returns an generator for the result if not this->is_distinct
+		 * @return result generator
+		 */
+		yield_pull<counted_binding> getRegularGenerator() const {
+			if (not is_distinct) {
+				if (not is_trivial_emtpy)
+					return regular_operator_tree->get();
 				else
-					throw std::domain_error("This Packackage holds a distinct Operator tree.");
-			}
+					return yield_pull<counted_binding>(
+							[&]([[maybe_unused]] yield_push<counted_binding> &yield) { return; });
+			} else
+				throw std::domain_error("This Packackage holds a distinct Operator tree.");
+		}
 
-			/**
-			 * Returns an generator for the result if this->is_distinct
-			 * @return result generator
-			 */
-			yield_pull<distinct_binding> getDistinctGenerator() const {
-				if (is_distinct) {
-					if (not is_trivial_emtpy)
-						return distinct_operator_tree->get();
-					else
-						return yield_pull<distinct_binding>(
-								[&]([[maybe_unused]] yield_push<distinct_binding> &yield) { return; });
-				} else
-					throw std::domain_error("This Packackage holds a non-distinct Operator tree.");
-			}
+		friend struct ::fmt::formatter<QueryExecutionPackage>;
 
-			/**
-			 * Returns an generator for the result if not this->is_distinct
-			 * @return result generator
-			 */
-			yield_pull<counted_binding> getRegularGenerator() const {
-				if (not is_distinct) {
-					if (not is_trivial_emtpy)
-						return regular_operator_tree->get();
-					else
-						return yield_pull<counted_binding>(
-								[&]([[maybe_unused]] yield_push<counted_binding> &yield) { return; });
-				} else
-					throw std::domain_error("This Packackage holds a distinct Operator tree.");
-			}
-
-			friend struct ::fmt::formatter<QueryExecutionPackage>;
-
-			private:
-			/**
-			 * Calculates the slice keys for the BoolHyperTrie from a basic graph pattern. If the result is clearly empty
-			 * an empty list of slice keys is returned.
-			 * @param bgps basic graph pattern of the query
-			 * @param trie BoolHyperTrie
-			 * @param termIndex intex for trie
-			 * @return slice keys for the BoolHyperTrie
-			 */
-			static std::vector<SliceKey_t> generateSliceKeys(const std::set<TriplePattern> &bgps,
-			                                                 const BoolHyperTrie &trie, const TermStore &termIndex) {
-				std::vector<SliceKey_t> slice_keys{};
-				for (const auto &op_key : bgps) {
-					SliceKey_t slice_key(3, std::nullopt);
-					bool no_slices = true;
-					for (const auto[pos, op_key_part] : enumerate(op_key)) {
-						if (std::holds_alternative<Term>(op_key_part))
-							try {
-								key_part_t ind = termIndex.at(std::get<Term>(op_key_part));
-								slice_key[pos] = {ind};
-							} catch ([[maybe_unused]] std::out_of_range &exc) {
-								// a keypart was not in the index so the result is zero anyways.
-								return {};
-							}
-						else
-							no_slices = false;
-					}
-					if (no_slices) {
-						// one triple without variables was not in storeF
-						if (not std::get<bool>(trie.get(slice_key)))
+	private:
+		/**
+		 * Calculates the slice keys for the BoolHyperTrie from a basic graph pattern. If the result is clearly empty
+		 * an empty list of slice keys is returned.
+		 * @param bgps basic graph pattern of the query
+		 * @param trie BoolHyperTrie
+		 * @param termIndex intex for trie
+		 * @return slice keys for the BoolHyperTrie
+		 */
+		static std::vector<SliceKey_t> generateSliceKeys(const std::set<TriplePattern> &bgps,
+		                                                 const BoolHyperTrie &trie, const TermStore &termIndex) {
+			std::vector<SliceKey_t> slice_keys{};
+			for (const auto &op_key : bgps) {
+				SliceKey_t slice_key(3, std::nullopt);
+				bool no_slices = true;
+				for (const auto[pos, op_key_part] : enumerate(op_key)) {
+					if (std::holds_alternative<Term>(op_key_part))
+						try {
+							key_part_t ind = termIndex.at(std::get<Term>(op_key_part));
+							slice_key[pos] = {ind};
+						} catch ([[maybe_unused]] std::out_of_range &exc) {
+							// a keypart was not in the index so the result is zero anyways.
 							return {};
-					} else
-						slice_keys.push_back(slice_key);
+						}
+					else
+						no_slices = false;
 				}
-
-				return slice_keys;
+				if (no_slices) {
+					// one triple without variables was not in storeF
+					if (not std::get<bool>(trie.get(slice_key)))
+						return {};
+				} else
+					slice_keys.push_back(slice_key);
 			}
-		};
 
-	} // namespace tnt::store::cache
-
-	template<>
-	struct fmt::formatter<tnt::store::cache::QueryExecutionPackage> {
-		template<typename ParseContext>
-		constexpr auto parse(ParseContext &ctx) { return ctx.begin(); }
-
-		template<typename FormatContext>
-		auto format(const tnt::store::cache::QueryExecutionPackage &p, FormatContext &ctx) {
-			return format_to(ctx.begin(),
-			                 " parsedSPARQL:     {}\n"
-			                 " is_distinct:      {}\n"
-			                 " is_trivial_emtpy: {}\n",
-			                 p.parsedSPARQL, p.is_distinct, p.is_trivial_emtpy);
-			// TODO: implement print for operator
+			return slice_keys;
 		}
 	};
+
+} // namespace tnt::store::cache
+
+template<>
+struct fmt::formatter<tnt::store::cache::QueryExecutionPackage> {
+	template<typename ParseContext>
+	constexpr auto parse(ParseContext &ctx) { return ctx.begin(); }
+
+	template<typename FormatContext>
+	auto format(const tnt::store::cache::QueryExecutionPackage &p, FormatContext &ctx) {
+		return format_to(ctx.begin(),
+		                 " parsedSPARQL:     {}\n"
+		                 " is_distinct:      {}\n"
+		                 " is_trivial_emtpy: {}\n",
+		                 p.parsedSPARQL, p.is_distinct, p.is_trivial_emtpy);
+		// TODO: implement print for operator
+	}
+};
 
 #endif // TNT_QUERYEXECUTIONPACKAGE_HPP
