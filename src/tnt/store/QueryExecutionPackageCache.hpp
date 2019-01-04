48b43f155978a3667ad7177f4283e67b0deeb73d
Alexander Bigerl <alexander[at]bigerl.eu>
2019-01-04 16:27:01 +0100

    prevent concurrent access to query execution packages (1)

--- src/tnt/store/QueryExecutionPackageCache.hpp
+++ src/tnt/store/QueryExecutionPackageCache.hpp
@@ -1,32 +1,78 @@
 #ifndef TNT_PARSEDSPARQLCACHES_HPP
 #define TNT_PARSEDSPARQLCACHES_HPP
 
-#include "tnt/util/SynchronizedCachedFactory.hpp"
+#include "tnt/http/TimeoutException.hpp"
 #include "tnt/store/QueryExecutionPackage.hpp"
+#include "tnt/util/SynchronizedCachedFactory.hpp"
 
 namespace {
-    using tnt::util::sync::SynchronizedCachedFactory;
-}
+	using tnt::http::TimeoutException;
+	using tnt::util::sync::SynchronizedCachedFactory;
+} // namespace
 
 namespace tnt::store::cache {
 
-    class QueryExecutionPackage_cache : public SynchronizedCachedFactory<std::string, QueryExecutionPackage> {
-    protected:
-        QueryExecutionPackage *construct(const std::string &key) override{
-            try {
-                return new QueryExecutionPackage{key, _trie, _termIndex};
-            } catch (std::invalid_argument &exc) {}
-            return nullptr;
-        }
+	class QueryExecutionPackage_cache : public SynchronizedCachedFactory<std::string, QueryExecutionPackage> {
+		std::timed_mutex set_timer_mutex;
+		std::chrono::system_clock::duration max_processing_time;
 
-        BoolHyperTrie &_trie;
-        TermStore &_termIndex;
+	protected:
+		QueryExecutionPackage *construct(const std::string &key) override {
+			try {
+				auto *qep = new QueryExecutionPackage{key, trie, termIndex};
+				return qep;
+			} catch ([[maybe_unused]] std::invalid_argument &exc) {
+			}
+			return nullptr;
+		}
 
-    public:
-        QueryExecutionPackage_cache(BoolHyperTrie &trie, TermStore &termIndex, uint capacity = 500) :
-                SynchronizedCachedFactory{capacity}, _trie{trie}, _termIndex{termIndex} {}
-    };
+		BoolHyperTrie &trie;
+		TermStore &termIndex;
 
-}
+	public: // TODO: use atomic config to initialize ot
+		QueryExecutionPackage_cache(BoolHyperTrie &trie, TermStore &termIndex, uint capacity = 500,
+		                            std::chrono::system_clock::duration max_processing_time = std::chrono::seconds(180))
+				: SynchronizedCachedFactory{capacity}, max_processing_time{max_processing_time}, trie{trie}, termIndex{
+				termIndex} {}
 
-#endif //TNT_PARSEDSPARQLCACHES_HPP
+		/**
+		 * Get a query execution package for this query.
+		 * @param query the query
+		 * @throws TimeoutException If it blocks for too long a TimeoutExecption is thrown. This may be due to another
+		 * request for the same query that is currently processed.
+		 * @return the query execution package. The std::timed_mutex queuing is set and must be unset after processing is
+		 * finished.
+		 */
+		std::shared_ptr<QueryExecutionPackage> get(const std::string &query) {
+			auto timeout = std::chrono::system_clock::now() + max_processing_time;
+
+			auto qep = this->SynchronizedCachedFactory::get(query);
+			if (qep->queuing.try_lock_until(timeout)) {
+
+				if (qep->processing.try_lock()) {
+					qep->queuing.unlock();
+					qep->timeout = timeout;
+
+					return qep;
+				} else {
+					qep->keep_result_timeout = timeout;
+
+					if (qep->processing.try_lock_until(timeout)) {
+						qep->queuing.unlock();
+						qep->timeout = timeout;
+
+						return qep;
+					} else {
+						qep->queuing.unlock();
+						throw TimeoutException{};
+					}
+				}
+			} else {
+				throw TimeoutException{};
+			}
+		}
+	}; // namespace tnt::store::cache
+
+} // namespace tnt::store::cache
+
+#endif // TNT_PARSEDSPARQLCACHES_HPP
