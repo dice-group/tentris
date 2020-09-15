b30e859f91c35b1bafcdfb087c34c4a3d03a7796
Alexander Bigerl <bigerl@mail.upb.de>
2020-09-15 18:52:32 +0200

    fixed TentrisTerminal.cpp build

--- src/exec/TentrisTerminal.cpp
+++ src/exec/TentrisTerminal.cpp
@@ -237,7 +237,8 @@ void commandlineInterface(QueryExecutionPackage_cache &querypackage_cache) {
 
 
 int main(int argc, char *argv[]) {
-	cfg = {argc, argv};
+	auto const_argv = const_cast<const char **>(argv);
+	cfg = TerminalConfig{argc, const_argv};
 	tentris::logging::init_logging(cfg.logstdout, cfg.logfile, cfg.logfiledir, cfg.loglevel);
 
 	TripleStore triplestore{};
