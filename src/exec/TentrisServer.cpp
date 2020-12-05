ee0cbdd7fde3fecf487f3bf5fb5e0e1cf12a093a
Alexander Bigerl <bigerl@mail.upb.de>
2020-12-05 11:24:20 +0100

    limit parallel connections to thread pool size

--- src/exec/TentrisServer.cpp
+++ src/exec/TentrisServer.cpp
@@ -12,7 +12,7 @@
 
 
 
-void bulkload(std::string triple_file, size_t bulksize) {
+void bulkload(const std::string &triple_file, size_t bulksize) {
 	namespace fs = std::filesystem;
 	using namespace fmt::literals;
 	using namespace tentris::logging;
@@ -33,6 +33,19 @@ void bulkload(std::string triple_file, size_t bulksize) {
 	// log the time it tool to load the file
 	log_duration(loading_start_time, loading_end_time);
 }
+
+struct tentris_restinio_traits : public	restinio::traits_t<
+		restinio::null_timer_manager_t,
+#ifdef DEBUG
+		restinio::shared_ostream_logger_t,
+#else
+		restinio::null_logger_t,
+#endif
+		restinio::router::express_router_t<>
+>{
+	static constexpr bool use_connection_count_limiter = true;
+};
+
 
 int main(int argc, char *argv[]) {
 	using namespace tentris::http;
@@ -75,21 +88,11 @@ int main(int argc, char *argv[]) {
 
 	// Launching a server with custom traits.
 
-	using traits_t =
-	restinio::traits_t<
-			restinio::null_timer_manager_t,
-#ifdef DEBUG
-			restinio::shared_ostream_logger_t,
-#else
-			null_logger_t,
-#endif
-			restinio::router::express_router_t<>
-	>;
-
 	log("SPARQL endpoint serving sparkling linked data treasures on {} threads at http://0.0.0.0:{}/sparql?query="_format(cfg.threads, cfg.port));
 
 	restinio::run(
-			restinio::on_thread_pool<traits_t>(cfg.threads)
+			restinio::on_thread_pool<tentris_restinio_traits>(cfg.threads)
+			        .max_parallel_connections(cfg.threads)
 					.address("0.0.0.0")
 					.port(cfg.port)
 					.request_handler(std::move(router))
