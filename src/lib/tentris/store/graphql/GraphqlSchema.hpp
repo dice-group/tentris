97390eaf6987039fcffaf1d16c87034ce7357c32
nkaralis <nkaralis@mail.uni-paderborn.de>
2021-02-25 17:05:08 +0100

    major changes in graphql schema, query parsing and subscript generation

--- src/lib/tentris/store/graphql/GraphqlSchema.hpp
+++ src/lib/tentris/store/graphql/GraphqlSchema.hpp
@@ -1,61 +1,155 @@
 #ifndef TENTRIS_GRAPHQLSCHEMA_HPP
 #define TENTRIS_GRAPHQLSCHEMA_HPP
 
-#include <utility>
-
-#include "tentris/store/graphql/internal/GraphqlDocumentVisitor.hpp"
-#include "tentris/store/graphql/internal/GraphqlObjectTypeVisitor.hpp"
-#include "tentris/store/graphql/internal/GraphqlParser.hpp"
+#include "tentris/store/graphql/GraphqlDocument.hpp"
+#include "tentris/store/graphql/internal/GraphqlDataStructures.hpp"
+#include "tentris/store/graphql/internal/GraphqlDirectiveVisitor.hpp"
 
 namespace tentris::store::graphql {
 
-    using namespace tentris::store::graphql::internal;
-	using FieldData = GraphqlObjectTypeVisitor::FieldData;
+	using ObjectData = ::tentris::store::graphql::internal::ObjectData;
+	using FieldData = ::tentris::store::graphql::internal::FieldData;
+
+	namespace internal {
+
+		class GraphqlFieldDefinitionVisitor : public facebook::graphql::ast::visitor::AstVisitor {
+
+		private:
+
+			GraphqlDirectiveVisitor directive_visitor{};
+
+        public:
+
+            std::string field_name{};
+			FieldData data{};
+
+			// visit functions
+
+            bool visitFieldDefinition(const facebook::graphql::ast::FieldDefinition &fieldDefinition) override {
+                field_name = fieldDefinition.getName().getValue();
+                return true;
+            }
+
+            bool visitNamedType(const facebook::graphql::ast::NamedType &namedType) override {
+                data.type_name = namedType.getName().getValue();
+                return false;
+            }
+
+            bool visitListType([[maybe_unused]] const facebook::graphql::ast::ListType &listType) override {
+                data.is_list = true;
+                return true;
+            }
+
+            bool visitNonNullType([[maybe_unused]] const facebook::graphql::ast::NonNullType &nonNullType) override {
+                if(data.is_list) // [ NamedType! ] !
+                    data.non_null_list_values = true;
+                else // NamedType!
+                    data.non_null = true;
+                return true;
+            }
+
+            // skip arguments -- todo
+            bool visitArgument([[maybe_unused]] const facebook::graphql::ast::Argument &argument) override {
+                return false;
+            }
+
+		    // we are currently only interested in the @uri(value: <uri>) directive
+            bool visitDirective(const facebook::graphql::ast::Directive &directive) override {
+                if(strcmp(directive.getName().getValue(), "uri") == 0) {
+                    directive.accept(&directive_visitor);
+					try {
+						data.uri = directive_visitor.args_vals.at("value");
+					}
+					catch (const std::out_of_range& e) {
+						throw std::runtime_error("The directive @uri take exactly one argument: `value`");
+					}
+                }
+                else
+                    throw std::runtime_error("Only the directive `@uri(value: String!)` is currently supported on field definitions");
+                return false;
+            }
+
+		};
+
+	} // tentris::store::graphql::internal
 
     class GraphqlSchema {
 
 	private:
 
-		struct ObjectData {
-			std::string uri;
-			std::map<std::string, FieldData> fields_data{};
-		};
+		using TypeName = std::string;
 
-        std::string query_type_name = "Query";
-		std::map<std::string, ObjectData> objects_data{};
+        TypeName query_type_name = "Query";
+		// for each object type stores its information
+        std::map<TypeName , ObjectData> objects_data{};
 
     public:
 
-        explicit GraphqlSchema(const std::string& path_to_schema_file) {
-            GraphqlDocumentVisitor doc_visitor{};
-			auto schema_doc = GraphqlParser::parseSchema(path_to_schema_file);
-			schema_doc->accept(&doc_visitor);
-			// iterate through and parse the object definitions of the the provided schema
-			for(const auto& obj_def : doc_visitor.getObjectDefs()) {
-				GraphqlObjectTypeVisitor obj_visitor{};
-				obj_def->accept(&obj_visitor);
-                ObjectData cur_obj_data{};
-                cur_obj_data.uri = obj_visitor.getUri();
-                cur_obj_data.fields_data = obj_visitor.getFieldsData();
-                objects_data[obj_visitor.getObjName()] = std::move(cur_obj_data);
+		GraphqlSchema() = default;
+
+		void load(const GraphqlDocument &document) {
+            // iterate over and parse the object definitions of the the provided schema
+            for(const auto& obj_def : document.getObjectDefs()) {
+				auto &obj_data = objects_data[obj_def->getName().getValue()];
+				// iterate over the field definitions of the current object definition
+				for(auto &field_def : obj_def->getFields()) {
+					internal::GraphqlFieldDefinitionVisitor field_def_visitor{};
+					field_def->accept(&field_def_visitor);
+					obj_data.fields_data[field_def_visitor.field_name] = field_def_visitor.data;
+				}
+				if(obj_def->getDirectives()) {
+					for(auto &directive : *(obj_def->getDirectives())) {
+						internal::GraphqlDirectiveVisitor directive_visitor{};
+                        if(strcmp(directive->getName().getValue(), "uri") == 0) {
+                            directive->accept(&directive_visitor);
+                            try {
+                                obj_data.uri = directive_visitor.args_vals.at("value");
+                            }
+                            catch (const std::out_of_range& e) {
+                                throw std::runtime_error("The directive @uri take exactly one argument: `value`");
+                            }
+                        }
+                        else
+                            throw std::runtime_error("Only the directive `@uri(value: String!)` "
+													 "is currently supported on object definitions");
+					}
+				}
 			}
 		}
 
-		[[nodiscard]] const std::string& getEntryPointUri(const std::string& field_name) const {
-			auto type_name = objects_data.at(query_type_name).fields_data.at(field_name).type;
-			return objects_data.at(type_name).uri;
-		}
+		// getters
 
-        [[nodiscard]] const std::string& getEntryPointType(const std::string& field_name) const {
-            return objects_data.at(query_type_name).fields_data.at(field_name).type;
+        [[nodiscard]] const std::string& getFieldUri(const std::string& field_name) const {
+            auto type_name = objects_data.at(query_type_name).fields_data.at(field_name).type_name;
+            return objects_data.at(type_name).uri;
         }
 
-        [[nodiscard]] const std::string& getFieldUri(const std::string& parent_type, const std::string& field_name) const {
+        [[nodiscard]] const std::string& getFieldUri(const std::string& field_name, const std::string& parent_type) const {
             return objects_data.at(parent_type).fields_data.at(field_name).uri;
         }
 
-        [[nodiscard]] const std::string& getFieldType(const std::string& parent_type, const std::string& field_name) const {
-            return objects_data.at(parent_type).fields_data.at(field_name).type;
+        [[nodiscard]] const std::string& getFieldType(const std::string& field_name) const {
+            return objects_data.at(query_type_name).fields_data.at(field_name).type_name;
+        }
+
+        [[nodiscard]] const std::string& getFieldType(const std::string& field_name, const std::string& parent_type) const {
+            return objects_data.at(parent_type).fields_data.at(field_name).type_name;
+        }
+
+        [[nodiscard]] bool fieldIsList(const std::string& field_name) const {
+            return objects_data.at(query_type_name).fields_data.at(field_name).is_list;
+        }
+
+        [[nodiscard]] bool fieldIsList(const std::string& field_name, const std::string& parent_type) const {
+            return objects_data.at(parent_type).fields_data.at(field_name).is_list;
+        }
+
+        [[nodiscard]] bool fieldIsNonNull(const std::string& field_name) const {
+            return objects_data.at(query_type_name).fields_data.at(field_name).non_null;
+        }
+
+        [[nodiscard]] bool fieldIsNonNull(const std::string& field_name, const std::string& parent_type) const {
+            return objects_data.at(parent_type).fields_data.at(field_name).non_null;
         }
 
 	};
--- src/lib/tentris/store/graphql/ParsedGraphql.hpp
+++ src/lib/tentris/store/graphql/ParsedGraphql.hpp
@@ -12,51 +12,48 @@
 #include <regex>
 #include <utility>
 
-#include <boost/algorithm/string.hpp>
 #include <Dice/einsum/internal/Subscript.hpp>
-#include <libgraphqlparser/Ast.h>
-#include <robin_hood.h>
+#include <Ast.h>
 
 #include "tentris/store/graphql/GraphqlSchema.hpp"
-#include "tentris/store/graphql/internal/GraphqlDocumentVisitor.hpp"
-#include "tentris/store/graphql/internal/GraphqlRootSelectionVisitor.hpp"
+#include "tentris/store/graphql/internal/GraphqlValueVisitor.hpp"
 
 namespace tentris::store::graphql {
 
-	namespace {
-		using Subscript = einsum::internal::Subscript;
-		using namespace fmt::literals;
-		using namespace facebook::graphql::ast;
-    }
+    using Subscript = einsum::internal::Subscript;
+    using namespace fmt::literals;
+    using namespace facebook::graphql::ast::visitor;
 
     class ParsedGraphql {
 
 	private:
 
-        std::vector<std::shared_ptr<Subscript>> subscripts{};
+		class RootSelectionVisitor;
+
+		std::vector<std::shared_ptr<Subscript>> subscripts{};
 		std::vector<std::vector<std::vector<std::string>>> slice_keys{};
 
 	public:
 
-        ParsedGraphql() = default;
+        ParsedGraphql() = delete;
 
-        explicit ParsedGraphql(const std::string& request_doc_path, const GraphqlSchema& schema) {
-            auto root_node = GraphqlParser::parseRequestDocument(request_doc_path);
-			GraphqlDocumentVisitor doc_visitor{};
-			root_node->accept(&doc_visitor);
-            auto query = doc_visitor.getQuery();
-            parseQuery(query, schema);
+        explicit ParsedGraphql(const OperationDefinition* query, const GraphqlSchema& schema) {
+            const auto& root_selection_set = query->getSelectionSet();
+            for(const auto& root_selection : root_selection_set.getSelections()) {
+                RootSelectionVisitor rsv{&schema};
+                std::vector<std::vector<std::string>> root_selection_slice_keys{};
+                std::map<std::string, std::string> field_type_map{};
+				try {
+					root_selection->accept(&rsv);
+				} catch(std::exception &e) {
+                    throw std::runtime_error(e.what());
+				}
+                subscripts.emplace_back(std::make_shared<Subscript>(rsv.operands_labels, rsv.result_labels));
+                slice_keys.emplace_back(std::move(rsv.fields));
+            }
 		}
 
-		explicit ParsedGraphql(const std::string& request_doc_path, const std::string& q_name, const GraphqlSchema& schema) {
-            auto root_node = GraphqlParser::parseRequestDocument(request_doc_path);
-            GraphqlDocumentVisitor doc_visitor{};
-            root_node->accept(&doc_visitor);
-            auto query = doc_visitor.getQuery(q_name);
-            parseQuery(query, schema);
-		}
-
-        [[nodiscard]] const std::vector<std::shared_ptr<Subscript>> &getSubscripts() const {
+		[[nodiscard]] const std::vector<std::shared_ptr<Subscript>> &getSubscripts() const {
             return subscripts;
         }
 
@@ -66,32 +63,113 @@ namespace tentris::store::graphql {
 
 	private:
 
-		void parseQuery(const facebook::graphql::ast::OperationDefinition* query, const GraphqlSchema& schema) {
-			const auto& root_selection_set = query->getSelectionSet();
-			for(const auto& root_selection : root_selection_set.getSelections()) {
-                GraphqlRootSelectionVisitor rsv{};
-				std::vector<std::vector<std::string>> root_selection_slice_keys{};
-				std::map<std::string, std::string> field_type_map{};
-				root_selection->accept(&rsv);
-				subscripts.push_back(rsv.getSubscript());
-				auto& fields = rsv.getFields();
-				auto root_field = fields.begin();
-				// get the uri of the type of the root field
-				root_selection_slice_keys.push_back({schema.getEntryPointUri((*root_field)[0].second)});
-				field_type_map[(*root_field)[0].second] = schema.getEntryPointType((*root_field)[0].second);
-				// iterate over the nested fields
-				for(auto iter = fields.begin() + 1; iter != fields.end(); iter++) {
-                    std::vector<std::string> field_slice_key{};
-					for(auto& entry : *iter) {
-						auto parent_type = field_type_map[entry.first];
-						field_slice_key.push_back(schema.getFieldUri(parent_type, entry.second));
-						field_type_map[entry.second] = schema.getFieldType(parent_type, entry.second);
-					}
-					root_selection_slice_keys.emplace_back(std::move(field_slice_key));
-				}
-				slice_keys.emplace_back(std::move(root_selection_slice_keys));
+		class RootSelectionVisitor : public AstVisitor {
+
+		private:
+
+			const GraphqlSchema *schema;
+
+		public:
+
+			// the result labels for the subscript
+            std::vector<Subscript::Label> result_labels{};
+			// the operands labels for the subscript
+            std::vector<std::vector<Subscript::Label>> operands_labels{};
+            // the uris of fields and scalar values arguments
+            std::vector<std::vector<std::string>> fields{};
+
+		public:
+
+			RootSelectionVisitor() = delete;
+
+			explicit RootSelectionVisitor(const GraphqlSchema *input_schema) : schema(input_schema) {}
+
+		private:
+
+			// helper fields for the visit functions
+            uint8_t bracket_counter = 0;
+            Subscript::Label next_label = 'a';
+            Subscript::Label field_label{};
+            std::vector<Subscript::Label> selection_set_label{}; // stack
+            std::vector<std::string> selection_set_root_type{}; // stack
+            std::string field_name{};
+			std::string field_type{};
+			internal::GraphqlValueVisitor value_visitor{};
+
+		public:
+
+			// visit functions
+
+			bool visitSelectionSet([[maybe_unused]] const SelectionSet &selectionSet) override {
+				// inner field
+				if(not selection_set_root_type.empty())
+					selection_set_root_type.push_back(schema->getFieldType(field_name, selection_set_root_type.back()));
+				//root field
+				else
+					selection_set_root_type.push_back(schema->getFieldType(field_name));
+                selection_set_label.push_back(field_label);
+				return true;
 			}
-		}
+
+			void endVisitSelectionSet([[maybe_unused]] const SelectionSet &selectionSet) override {
+                selection_set_root_type.pop_back();
+                selection_set_label.pop_back();
+			}
+
+			bool visitField(const Field &field) override {
+                field_name = field.getName().getValue();
+                std::vector<std::string> new_field{};
+                field_label = next_label++;
+                std::vector<Subscript::Label> operand_labels{};
+                // inner - leaf field
+                if(not fields.empty()) {
+					field_type = schema->getFieldType(field_name, selection_set_root_type.back());
+                    new_field.push_back(schema->getFieldUri(field_name, selection_set_root_type.back()));
+					operands_labels.push_back({'['});
+                    bracket_counter++;
+                    operand_labels.emplace_back(selection_set_label.back());
+                    operand_labels.emplace_back(field_label);
+                    // leaf field
+                    if (not field.getSelectionSet())
+                        result_labels.emplace_back(field_label);
+                }
+                // root field
+                else {
+                    field_type = schema->getFieldType(field_name);
+					new_field.push_back(schema->getFieldUri(field_name));
+					operand_labels.emplace_back(field_label);
+					// root & leaf field
+					if (not field.getSelectionSet()) {
+						operand_labels.emplace_back(++next_label);
+						result_labels.emplace_back(next_label);
+					}
+				}
+                operands_labels.emplace_back(std::move(operand_labels));
+                fields.emplace_back(std::move(new_field));
+				return true;
+			}
+
+			void endVisitField([[maybe_unused]] const Field &field) override {
+                if(bracket_counter > 0) {
+                    operands_labels.push_back({']'});
+                    bracket_counter--;
+                }
+			}
+
+			bool visitArgument(const Argument &argument) override {
+				const auto &arg_name = argument.getName().getValue();
+				std::vector<std::string> new_field{};
+				operands_labels.emplace_back(std::vector<Subscript::Label>{field_label});
+				new_field.push_back(schema->getFieldUri(arg_name, field_type));
+				argument.getValue().accept(&value_visitor);
+				new_field.push_back(value_visitor.getValue());
+				fields.emplace_back(std::move(new_field));
+				return false;
+			}
+
+			// todo directives, fragments
+
+		};
 
 	};
 
