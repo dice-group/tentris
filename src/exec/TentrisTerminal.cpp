b131368101cc993f311ef72358c460697e82fd59
Alexander Bigerl <bigerl@mail.upb.de>
2021-03-26 09:46:31 +0100

    cleanup for release

--- src/exec/TentrisTerminal.cpp
+++ src/exec/TentrisTerminal.cpp
@@ -2,23 +2,23 @@
 #include <csignal>
 #include <filesystem>
 #include <fstream>
-#include <cstdint>
 #include <chrono>
 #include <thread>
-
-#include "config/TerminalConfig.hpp"
 
 #include <tentris/store/QueryExecutionPackage.hpp>
 #include <tentris/store/QueryExecutionPackageCache.hpp>
 #include <tentris/store/TripleStore.hpp>
 #include <tentris/util/LogHelper.hpp>
 #include <tentris/tensor/BoolHypertrie.hpp>
+#include <tentris/http/QueryResultState.hpp>
 
 #include <fmt/core.h>
 #include <fmt/format.h>
-#include <fmt/ranges.h>
 #include <itertools.hpp>
-#include "tentris/http/QueryResultState.hpp"
+
+
+#include "config/TerminalConfig.hpp"
+#include "VersionStrings.hpp"
 
 using namespace tentris::store;
 using namespace tentris::logging;
@@ -241,6 +241,8 @@ void commandlineInterface(QueryExecutionPackage_cache &querypackage_cache) {
 int main(int argc, char *argv[]) {
 	cfg = TerminalConfig{argc, argv};
 	tentris::logging::init_logging(cfg.logstdout, cfg.logfile, cfg.logfiledir, cfg.loglevel);
+
+	logsink() << fmt::format("Running {} with {}", tentris_version_string, hypertrie_version_string) << std::endl;
 
 	TripleStore triplestore{};
 
