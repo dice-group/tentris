ed229010462f558411f626f5a182afdb245d3025
nkaralis <nkaralis@mail.uni-paderborn.de>
2021-06-25 19:36:42 +0300

    logging

--- src/lib/tentris/store/cache/GraphQLExecutionPackage.cpp
+++ src/lib/tentris/store/cache/GraphQLExecutionPackage.cpp
@@ -19,31 +19,44 @@ namespace tentris::store::cache {
 				if (operand_labels == opt_begin or operand_labels == opt_end)
 					continue;
 				// root field
-				if (pos == 0)
-					operands.push_back(triple_store.resolveGQLObjectType(features[pos].back()));
+				if (pos == 0) {
+					logDebug(fmt::format("Slicing for root field: `{}`", features[pos]));
+					auto field_operand = triple_store.resolveGQLObjectType(features[pos].back());
+					// argument id
+					try {
+						field_operand = triple_store.resolveGQLArgumentID(sub_query.id_arguments.at(pos), field_operand);
+					} catch (...) {}
+					operands.push_back(std::move(field_operand));
+				}
 				// arguments, inner/leaf fields and inline fragments
 				else {
 					// fields
-					if (operand_labels.size() == 2)
-						operands.push_back(triple_store.resolveGQLField(features[pos].back()));
-					else {
+					if (operand_labels.size() == 2) {
+						logDebug(fmt::format("Slicing for inner/leaf field: `{}`", features[pos]));
+						auto field_operand = triple_store.resolveGQLField(features[pos].back());
+						// id field
+                        try {
+                            field_operand = triple_store.resolveGQLArgumentID(sub_query.id_arguments.at(pos), field_operand);
+                        } catch (...) {}
+                        operands.push_back(std::move(field_operand));
+					} else {
 						// arguments
-						if (features[pos].size() == 3)
+						if (features[pos].size() == 3) {
+                            logDebug(fmt::format("Slicing for argument: `{}`. Argument value: `{}`",
+												 features[pos][0], features[pos][1]));
 							operands.push_back(triple_store.resolveGQLArgument(features[pos][0],
 																			   features[pos][1],
 																			   features[pos][2]));
-						// inline fragment and type filter
-						else
+						} else { // inline fragment and type filter
+                            logDebug(fmt::format("Slicing for type : `{}`", features[pos]));
 							operands.push_back(triple_store.resolveGQLObjectType(features[pos].back()));
+						}
 					}
 				}
 				pos++;
 			}
 			subscripts.push_back(std::make_shared<Subscript>(sub_query.operands_labels, sub_query.result_labels));
 			logDebug(fmt::format("Subscript #{}: {}", subscripts.size(), subscripts.back()->to_string()));
-			// filter id arguments
-			for (const auto &[op_pos, value] : sub_query.id_arguments)
-				operands[op_pos] = triple_store.resolveGQLArgumentID(value, operands[op_pos]);
 			all_operands.push_back(std::move(operands));
 		}
 	}
